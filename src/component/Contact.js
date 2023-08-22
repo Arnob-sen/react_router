464b1aeeef4521a1ab7a6b37f30851317dc47607
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-23 00:37:02 +0600

    showing contact page to about page in 2sec using navigate

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Contact =()=>
 {
+    const navigate=useNavigate();
+    
+
+   
+    setTimeout(()=>{
+        navigate('/about')
+
+      //  props.history.push("/about")
+    },2000)
     return(
         <div className="ui raised very padded text container segment"
         style={{marginTop:'80px'}}>
